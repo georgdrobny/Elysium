6763eb7306cf9ec742651659bb89baa427c673c0
Andre Bossard <anbossar@microsoft.com>
2022-04-12 15:23:12 +0200

    install helm package

--- pipelines/scripts/connect-to-cluster.sh
+++ pipelines/scripts/connect-to-cluster.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 PRODUCT=$1
 ENVIRONMENT=$2
+ACR=$3
 NAME=`az aks list  --query "[?tags.product == '$PRODUCT' && tags.environment == '$ENVIRONMENT'].name|[0]" -o json | tr -d \"`
 RESOURCE_GROUP=`az aks list  --query "[?tags.product == '$PRODUCT' && tags.environment == '$ENVIRONMENT'].resourceGroup|[0]" -o json | tr -d \"`
 az aks get-credentials --resource-group $RESOURCE_GROUP --name $NAME --admin
+az aks update -n $NAME -g $RESOURCE_GROUP --attach-acr $ACR
--- pipelines/scripts/install-helm-chart.sh
+++ pipelines/scripts/install-helm-chart.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
-
 ACR=$1
-CHART_VERSION=$2
-APP_VERSION=$3
+CHART_VERSION=$3
+PRODUCT=$4
+ENVIRONMENT=$5
+RELEASE_NAME=$6
+NAMESPACE=$7
+
 echo ACR=$ACR CHART_VERSION=$CHART_VERSION APP_VERSION=$APP_VERSION
 helm version
 export HELM_EXPERIMENTAL_OCI=1
 USER_NAME="00000000-0000-0000-0000-000000000000"
 PASSWORD=$(az acr login --name $ACR --expose-token --output tsv --query accessToken)
 helm registry login $ACR.azurecr.io --username $USER_NAME --password $PASSWORD
-helm package charts/elysium --version $CHART_VERSION --app-version $APP_VERSION --debug
-helm push elysium*.tgz oci://$ACR.azurecr.io/helm
+k create namespace $NAMESPACE
+helm upgrade --install $RELEASE_NAME oci://$ACR_NAME.azurecr.io/helm/elysium \ 
+    --version $CHART_VERSION \
+    --namespace $NAMESPACE \
+    --create-namespace \
+    --verify \
+    -f environments/$ENVIRONMENT/values.yaml
