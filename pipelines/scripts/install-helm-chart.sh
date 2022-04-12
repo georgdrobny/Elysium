95c14eb209ef306e4e45aea62987804cc955e75a
Andre Bossard <anbossar@microsoft.com>
2022-04-12 16:54:50 +0200

    helm fixeds

--- pipelines/scripts/install-helm-chart.sh
+++ pipelines/scripts/install-helm-chart.sh
@@ -6,13 +6,13 @@ ENVIRONMENT=$5
 RELEASE_NAME=$6
 NAMESPACE=$7
 
-echo ACR=$ACR CHART_VERSION=$CHART_VERSION APP_VERSION=$APP_VERSION
+echo ACR=$ACR CHART_VERSION=$CHART_VERSION PRODUCT=$PRODUCT ENVIRONMENT=$ENVIRONMENT RELEASE_NAME=$RELEASE_NAME NAMESPACE=$NAMESPACE
 helm version
 export HELM_EXPERIMENTAL_OCI=1
 USER_NAME="00000000-0000-0000-0000-000000000000"
 PASSWORD=$(az acr login --name $ACR --expose-token --output tsv --query accessToken)
 helm registry login $ACR.azurecr.io --username $USER_NAME --password $PASSWORD
-helm upgrade $RELEASE_NAME oci://$ACR_NAME.azurecr.io/helm/elysium \ 
+helm upgrade $RELEASE_NAME oci://$ACR.azurecr.io/helm/elysium \ 
     --install \
     --version $CHART_VERSION \
     --namespace $NAMESPACE \
